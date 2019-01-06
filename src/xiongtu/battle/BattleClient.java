f54f16e787e6f018a4d13e6a1cc36511d6ff6e32
tom110 <254449149@qq.com>
2019-01-06 20:03:46 +0800

    version 0.1

---
+++ src/xiongtu/battle/BattleClient.java
@@ -0,0 +1,71 @@
+package xiongtu.battle;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class BattleClient extends Frame {
+
+    public static final int GAME_WIDTH=1600;
+    public static final int GAME_HEIGTH=1600;
+
+    int x=50,y=50;
+
+    Image image=null;
+
+    public void update(Graphics g){
+        if(image==null){
+            image=this.createImage(GAME_WIDTH,GAME_HEIGTH);
+        }
+        Graphics graphics=image.getGraphics();
+//        Color color=graphics.getColor();
+        graphics.setColor(Color.blue);
+        graphics.fillRect(0,0,GAME_WIDTH,GAME_HEIGTH);
+        paint(graphics);
+        g.drawImage(image,0,0,null);
+    }
+
+    public void paint(Graphics graphics){
+//        Color color=graphics.getColor();
+        graphics.setColor(Color.red);
+        graphics.fillOval(x,y,100,100);
+//        graphics.setColor(color);
+        y=y+1;
+        new Thread(new PaintThread()).start();
+    }
+
+    public void lauchFrame(){
+        this.setLocation(400,300);
+        this.setSize(GAME_WIDTH,GAME_HEIGTH);
+        this.setTitle("BattleClient");
+        this.setBackground(Color.blue);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String [] args){
+        BattleClient battleClient=new BattleClient();
+        battleClient.lauchFrame();
+    }
+
+    private class PaintThread implements Runnable{
+
+        @Override
+        public void run() {
+            while(true){
+                repaint();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
